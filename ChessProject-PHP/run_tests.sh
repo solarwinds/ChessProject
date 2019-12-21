bc1face2a46f115415b84afbaec5a4d7eaf2a581
Lohoris@Gloriana <lohoris@goblinsama.com>
2019-12-21 17:03:23 +0100

    result_tests_prev.txt

--- ChessProject-PHP/run_tests.sh
+++ ChessProject-PHP/run_tests.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
+if [ -f result_tests.txt ]; then
+	mv result_tests.txt result_tests_prev.txt
+fi
 ./vendor/bin/phpunit --bootstrap vendor/autoload.php tests/ >result_tests.txt
 mate result_tests.txt
