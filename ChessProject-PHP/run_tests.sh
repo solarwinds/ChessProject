f50f902cc6b19a2f8ce9d541dc49cee0232e24e0
Lohoris@Gloriana <lohoris@goblinsama.com>
2019-12-21 18:04:39 +0100

    fix for TextMate

--- ChessProject-PHP/run_tests.sh
+++ ChessProject-PHP/run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ -f result_tests.txt ]; then
-	mv result_tests.txt result_tests_prev.txt
+	cp result_tests.txt result_tests_prev.txt
+	truncate -s0 result_tests.txt
 fi
 ./vendor/bin/phpunit --bootstrap vendor/autoload.php tests/ >result_tests.txt
 mate result_tests.txt
