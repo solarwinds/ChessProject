210733ff96deb66a395c7147cc8a3ed37a09d198
Lohoris@Gloriana <lohoris@goblinsama.com>
2019-12-22 19:52:22 +0100

    Pawn->validPattern() implementing captures

--- ChessProject-PHP/src/Pawn.php
+++ ChessProject-PHP/src/Pawn.php
@@ -12,19 +12,31 @@ class Pawn extends Piece
     }
     
     protected function validPattern (int $newX, int $newY) {
-        // TODO: captures
         // TODO: double‑move
         
-        if ( $newX != $this->xCoordinate )
+        if ( $this->goingUp() && $newY!=$this->yCoordinate+1 )
+            return FALSE;
+    
+        if ( $this->goingDown() && $newY!=$this->yCoordinate-1 )
             return FALSE;
         
-        if ( $this->goingUp() && $newY==$this->yCoordinate+1 )
-            return TRUE;
+        $dest_cell = $this->chessBoard->getCell($newX,$newY);
         
-        if ( $this->goingDown() && $newY==$this->yCoordinate-1 )
-            return TRUE;
+        if ( $dest_cell === ChessBoard::EMPTY ) {
+            // Moving into an empty cell, checking for a normal moving pattern
+            
+            if ( $newX != $this->xCoordinate )
+                return FALSE;
+        }
+        else {
+            // Moving into an occupied cell, checking for a capture pattern
+            // NOTE: we won't check here for the colour of the captured Piece, this will be done in Piece's validMove()
+            
+            if ( ($newX != $this->xCoordinate+1) && ($newX != $this->xCoordinate-1) )
+                return FALSE;
+        }
         
-        return FALSE;
+        return TRUE;
     }
     protected function validPath (int $newX, int $newY) {
         // TODO: double‑move
