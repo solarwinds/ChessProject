42511c3e0ed9fe335ab691b7fb9f40f75174e4d2
Lohoris@Gloriana <lohoris@goblinsama.com>
2019-12-22 14:59:45 +0100

    PawnTest rename: _testSubject --> _testWhite (preparation for capture tests)

--- ChessProject-PHP/tests/PawnTest.php
+++ ChessProject-PHP/tests/PawnTest.php
@@ -12,32 +12,32 @@ class PawnTest extends \PHPUnit_Framework_TestCase
     private $_chessBoard;
     
     /** @var  Pawn */
-    private $_testSubject;
+    private $_testWhite;
     
     /** @var  Pawn */
     private $_testBlack;
     
     protected function setUp () {
         $this->_chessBoard = new ChessBoard();
-        $this->_testSubject = new Pawn(PieceColorEnum::WHITE());
+        $this->_testWhite = new Pawn(PieceColorEnum::WHITE());
         $this->_testBlack = new Pawn(PieceColorEnum::BLACK());
     }
     
     public function testChessBoard_Add_Sets_XCoordinate () {
-        $this->_chessBoard->add($this->_testSubject, 6, 3);
-        $this->assertEquals(6, $this->_testSubject->getXCoordinate());
+        $this->_chessBoard->add($this->_testWhite, 6, 3);
+        $this->assertEquals(6, $this->_testWhite->getXCoordinate());
     }
     public function testChessBoard_Add_Sets_YCoordinate () {
-        $this->_chessBoard->add($this->_testSubject, 6, 3);
-        $this->assertEquals(3, $this->_testSubject->getYCoordinate());
+        $this->_chessBoard->add($this->_testWhite, 6, 3);
+        $this->assertEquals(3, $this->_testWhite->getYCoordinate());
     }
     
     public function testPawn_Move_IllegalDestination_High () {
         $height = $this->_chessBoard->getSquareHeight();
-        $this->_chessBoard->add($this->_testSubject, 0, $height-1);
+        $this->_chessBoard->add($this->_testWhite, 0, $height-1);
         
         $this->expectException(\InvalidArgumentException::class);
-        $this->_testSubject->move(0, $height);
+        $this->_testWhite->move(0, $height);
     }
     public function testPawn_Move_IllegalDestination_Low () {
         $this->_chessBoard->add($this->_testBlack, 0, 0);
@@ -46,26 +46,26 @@ class PawnTest extends \PHPUnit_Framework_TestCase
         $this->_testBlack->move(0, -1);
     }
     public function testPawn_Move_IllegalCoordinates_Right_DoesNotMove () {
-        $this->_chessBoard->add($this->_testSubject, 6, 3);
+        $this->_chessBoard->add($this->_testWhite, 6, 3);
         $this->expectException(\InvalidArgumentException::class);
-        $this->_testSubject->move(7, 3);
+        $this->_testWhite->move(7, 3);
     }
     public function testPawn_Move_IllegalCoordinates_Left_DoesNotMove () {
-        $this->_chessBoard->add($this->_testSubject, 6, 3);
+        $this->_chessBoard->add($this->_testWhite, 6, 3);
         $this->expectException(\InvalidArgumentException::class);
-        $this->_testSubject->move(5, 3);
+        $this->_testWhite->move(5, 3);
     }
     
     public function testPawn_Move_White_Upwards () {
-        $this->_chessBoard->add($this->_testSubject, 6, 2);
-        $this->_testSubject->move(6, 3);
-        $this->assertEquals(6, $this->_testSubject->getXCoordinate());
-        $this->assertEquals(3, $this->_testSubject->getYCoordinate());
+        $this->_chessBoard->add($this->_testWhite, 6, 2);
+        $this->_testWhite->move(6, 3);
+        $this->assertEquals(6, $this->_testWhite->getXCoordinate());
+        $this->assertEquals(3, $this->_testWhite->getYCoordinate());
     }
     public function testPawn_Move_White_Downwards () {
-        $this->_chessBoard->add($this->_testSubject, 6, 3);
+        $this->_chessBoard->add($this->_testWhite, 6, 3);
         $this->expectException(\InvalidArgumentException::class);
-        $this->_testSubject->move(6, 2);
+        $this->_testWhite->move(6, 2);
     }
     public function testPawn_Move_Black_Downwards () {
         $this->_chessBoard->add($this->_testBlack, 4, 4);
