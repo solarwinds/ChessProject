bbb59b1de85f318a7afa72087f8a16b147c2db42
garywhite <g.z.white@mac.com>
2020-03-02 22:05:21 +0000

    Test that legal moves are reflected on the chessboard. Some test renaming for consistency.

--- ChessProject-Java/src/test/java/com/solarwindsmsp/chess/PawnTest.java
+++ ChessProject-Java/src/test/java/com/solarwindsmsp/chess/PawnTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class PawnTest {
 
@@ -32,7 +33,7 @@ public class PawnTest {
     }
 
     @Test
-    public void test_Pawn_Move_IllegalCoordinates_Right_DoesNotMove() {
+    public void test_Pawn_Move_IllegalCoordinates_Right_Has_No_Effect() {
         chessBoard.add(testSubject, 6, 3, PieceColor.BLACK);
         testSubject.move(MovementType.MOVE, 7, 3);
 
@@ -41,7 +42,7 @@ public class PawnTest {
     }
 
     @Test
-    public void test_Pawn_Move_IllegalCoordinates_Left_DoesNotMove() {
+    public void test_Pawn_Move_IllegalCoordinates_Left_Has_No_Effect() {
         chessBoard.add(testSubject, 6, 3, PieceColor.BLACK);
         testSubject.move(MovementType.MOVE, 4, 3);
 
@@ -50,16 +51,19 @@ public class PawnTest {
     }
 
     @Test
-    public void test_Black_Pawn_Move_LegalCoordinates_Forward_UpdatesCoordinates() {
+    public void test_Black_Pawn_Move_LegalCoordinates_Forward_Updates_Coordinates_And_Moves() {
         chessBoard.add(testSubject, 6, 6, PieceColor.BLACK);
         testSubject.move(MovementType.MOVE, 6, 5);
 
         assertEquals(6, testSubject.getXCoordinate());
         assertEquals(5, testSubject.getYCoordinate());
+
+        assertNull(chessBoard.getPieces()[6][6]);
+        assertEquals("p", chessBoard.getPieces()[6][5].toString());
     }
 
     @Test
-    public void test_White_Pawn_Move_LegalCoordinates_Forward_UpdatesCoordinates() {
+    public void test_White_Pawn_Move_LegalCoordinates_Forward_Updates_Coordinates_And_Moves() {
         Pawn whitePawn = new Pawn(PieceColor.WHITE);
         whitePawn.setChessBoard(chessBoard);
         chessBoard.add(whitePawn, 6, 1, PieceColor.WHITE);
@@ -71,11 +75,14 @@ public class PawnTest {
         assertEquals(6, whitePawn.getXCoordinate());
         assertEquals(2, whitePawn.getYCoordinate());
 
+        assertNull(chessBoard.getPieces()[6][1]);
+        assertEquals("P", chessBoard.getPieces()[6][2].toString());
+
         System.out.println(chessBoard);
     }
 
     @Test
-    public void test_Black_Pawn_Move_IllegalTargetCoordinates_Forward_DoesNotMove() {
+    public void test_Black_Pawn_Move_IllegalTargetCoordinates_Forward_Has_No_Effect() {
         chessBoard.add(testSubject, 6, 0, PieceColor.BLACK);
         testSubject.move(MovementType.MOVE, 6, -1);
 
@@ -84,7 +91,7 @@ public class PawnTest {
     }
 
     @Test
-    public void test_White_Pawn_Move_IllegalTargetCoordinates_Forward_DoesNotMove() {
+    public void test_White_Pawn_Move_IllegalTargetCoordinates_Forward_Has_No_Effect() {
         Pawn whitePawn = new Pawn(PieceColor.WHITE);
         whitePawn.setChessBoard(chessBoard);
         chessBoard.add(whitePawn, 6, 7, PieceColor.WHITE);
