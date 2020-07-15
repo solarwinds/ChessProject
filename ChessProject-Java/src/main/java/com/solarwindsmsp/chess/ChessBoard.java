22f7b87e8e0a69f646236bcb38ee0531cf0c5c18
dragos <dragoslixi@yahoo.com>
2020-07-15 14:00:43 +0300

    dlixandru - tests fixed

--- ChessProject-Java/src/main/java/com/solarwindsmsp/chess/ChessBoard.java
+++ ChessProject-Java/src/main/java/com/solarwindsmsp/chess/ChessBoard.java
@@ -12,10 +12,28 @@ public class ChessBoard {
     }
 
     public void addPiece(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+        if(isLegalBoardPosition(xCoordinate, yCoordinate) &&
+                !existsAnotherPieceWithSameColor(xCoordinate,yCoordinate,pieceColor)) {
+            pawn.setXCoordinate(xCoordinate);
+            pawn.setYCoordinate(yCoordinate);
+            pawn.setChessBoard(this);
+            pieces[xCoordinate][yCoordinate] = pawn;
+        }
+        else {
+            pawn.setXCoordinate(-1);
+            pawn.setYCoordinate(-1);
+        }
+    }
+
+    private boolean existsAnotherPieceWithSameColor(int xCoordinate, int yCoordinate, PieceColor pieceColor) {
+        return pieces[xCoordinate][yCoordinate] != null && pieces[xCoordinate][yCoordinate].getPieceColor().equals(pieceColor);
     }
 
     public boolean isLegalBoardPosition(int xCoordinate, int yCoordinate) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+        return isLegalCoordinate(xCoordinate) && isLegalCoordinate(yCoordinate);
+    }
+
+    private boolean isLegalCoordinate(int coordinate) {
+        return coordinate >= 0 && coordinate <= 6;
     }
 }
