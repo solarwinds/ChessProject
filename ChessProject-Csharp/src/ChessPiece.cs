c0bd4891d1c07af965a0e4448b8ceee7fd306f25
Taylor Rogers <taylor.rogers71@gmail.com>
2020-10-04 16:55:08 -0700

    Create abstract ChessPiece class

---
+++ ChessProject-Csharp/src/ChessPiece.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SolarWinds.MSP.Chess
+{
+    public abstract class ChessPiece
+    {
+        protected PieceColor pieceColor;
+        
+        public ChessBoard ChessBoard { get; set; }
+
+        public int XCoordinate { get; set; }
+        
+        public int YCoordinate { get; set; }
+
+        public PieceColor PieceColor
+        {
+            get { return pieceColor; }
+            private set { pieceColor = value; }
+        }
+
+        public abstract void Move(MovementType movementType, int newX, int newY);
+
+        public override string ToString()
+        {
+            return CurrentPositionAsString();
+        }
+
+        protected string CurrentPositionAsString()
+        {
+            return string.Format("Current X: {1}{0}Current Y: {2}{0}ChessPiece Color: {3}", Environment.NewLine, XCoordinate, YCoordinate, PieceColor);
+        }
+
+    }
+}
