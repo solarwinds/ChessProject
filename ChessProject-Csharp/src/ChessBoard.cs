a0d01bb0740834508053d2140eaf0e1d3b534349
Taylor Rogers <taylor.rogers71@gmail.com>
2020-10-04 17:00:21 -0700

    Begin Abstracting ChessBoard

--- ChessProject-Csharp/src/ChessBoard.cs
+++ ChessProject-Csharp/src/ChessBoard.cs
@@ -6,14 +6,18 @@ namespace SolarWinds.MSP.Chess
     {
         public static readonly int MaxBoardWidth = 7;
         public static readonly int MaxBoardHeight = 7;
-        private Pawn[,] pieces;
+        private object[,] pieces;
 
         public ChessBoard ()
         {
-            pieces = new Pawn[MaxBoardWidth, MaxBoardHeight];
+            pieces = new object[MaxBoardHeight, MaxBoardWidth];
+        }
+        public ChessBoard (object[,] board)
+        {
+            pieces = board;
         }
 
-        public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor)
+        public void Add<T>(T piece, int xCoordinate, int yCoordinate, PieceColor pieceColor)
         {
             throw new NotImplementedException("Need to implement ChessBoard.Add()");
         }
@@ -21,6 +25,21 @@ namespace SolarWinds.MSP.Chess
         public bool IsLegalBoardPosition(int xCoordinate, int yCoordinate)
         {
             throw new NotImplementedException("Need to implement ChessBoard.IsLegalBoardPosition()");
+        }
+
+        // public void Print()
+        // {
+        //     for (int row = 0; row < pieces.GetLength(0); row++){
+        //         for (int col = 0; col < pieces.GetLength(1); col++){
+        //             pieces[row, col].Print();
+        //         }
+        //     }
+        // }
+
+        protected string CurrentBoardAsString()
+        {
+
+            return string.Format("=========", Environment.NewLine, XCoordinate, YCoordinate, PieceColor);
         }
     }
 }
