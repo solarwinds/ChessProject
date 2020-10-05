25c92fb5561d5be5ed6ed0f35a80d082fea2e6df
Taylor Rogers <taylor.rogers71@gmail.com>
2020-10-05 10:43:34 -0700

    AvailablePieces refactor for tests

--- ChessProject-Csharp/src/AvailablePieces.cs
+++ ChessProject-Csharp/src/AvailablePieces.cs
@@ -4,97 +4,160 @@ namespace SolarWinds.MSP.Chess
 {
     public class AvailablePieces
     {
+        public object this[string propertyName] 
+        {
+            get {
+                return typeof(AvailablePieces).GetProperty(propertyName).GetValue(this);
+            }
+            set {
+                typeof(AvailablePieces).GetProperty(propertyName).SetValue(this, value);
+
+            }
+
+        }
 
         public AvailablePieces()
         {
-            this.pawns = 8;
-            this.rooks = 2;
-            this.knights = 2;
-            this.bishops = 2;
-            this.queens = 1;
-            this.kings = 1;
+            this.Black = new PossiblePieces();
+            this.White = new PossiblePieces();
         }
 
-        public AvailablePieces(int pawns, int rooks, int knights, int bishops, int queens, int kings)
+        public AvailablePieces(
+            int blackPawn = 8, int blackRook = 2, int blackKnight = 2, int blackBishop = 2, int blackQueen = 1, int blackKing = 1,
+            int whitePawn = 8, int whiteRook = 2, int whiteKnight = 2, int whiteBishop = 2, int whiteQueen = 1, int whiteKing = 1
+        )
         {
-            this.pawns = pawns;
-            this.rooks = rooks;
-            this.knights = knights;
-            this.bishops = bishops;
-            this.queens = queens;
-            this.kings = kings;
+            this.Black = new PossiblePieces(blackPawn, blackRook, blackKnight, blackBishop, blackQueen, blackKing);
+            this.White = new PossiblePieces(whitePawn, whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing);
         }
 
-        private int pawns;
+        private PossiblePieces black;
 
-        public int Pawns
+        private PossiblePieces white;
+
+        public PossiblePieces Black
         {
-            get { return pawns; }
-            set { 
-                if (value == (pawns - 1)){
-                    pawns--;
-                }
+            get { return black; }
+            private set { black = value; }
+        }
+
+        public PossiblePieces White
+        {
+            get { return white; }
+            private set { white = value; }
+        }
+
+        public bool Decrement(string color, string pieceType)
+        {
+            if (color != "Black" && color != "White")
+            {
+                throw new ArgumentException("Color must be either 'Black' or 'White'");
+            }
+            PossiblePieces colorPieceCounts = (PossiblePieces)this[color];
+            int curr;
+            try
+            {
+                curr = (int)colorPieceCounts[pieceType];
+            } catch (NullReferenceException)
+            {
+                throw new ArgumentException("Cannot decrement an unrecognized pieceType. Got: {0}", pieceType);
+            }
+            Console.WriteLine("Decrementing {0} {1} (Currently: {2})...", color, pieceType, curr);
+            if (curr > 0)
+            {
+                colorPieceCounts[pieceType] = --curr;
+                Console.WriteLine("Decremented {0}", colorPieceCounts[pieceType]);
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
-        private int rooks;
-
-        public int Rooks
+        public class PossiblePieces 
         {
-            get { return rooks; }
-            set { 
-                if (value == (rooks - 1)){
-                    rooks--;
+            public object this[string propertyName] 
+            {
+                get {
+                    return typeof(PossiblePieces).GetProperty(propertyName).GetValue(this);
                 }
+                set {
+                    typeof(PossiblePieces).GetProperty(propertyName).SetValue(this, value);
+
+                }
+
             }
-        }
 
-        private int knights;
-
-        public int Knights
-        {
-            get { return knights; }
-            set { 
-                if (value == (knights - 1)){
-                    knights--;
-                }
+            public PossiblePieces()
+            {
+                this.pawn = 8;
+                this.rook = 2;
+                this.knight = 2;
+                this.bishop = 2;
+                this.queen = 1;
+                this.king = 1;
             }
-        }
 
-        private int bishops;
-
-        public int Bishops
-        {
-            get { return bishops; }
-            set { 
-                if (value == (bishops - 1)){
-                    bishops--;
-                }
+            public PossiblePieces(int pawn, int rook, int knight, int bishop, int queen, int king)
+            {
+                this.pawn = pawn;
+                this.rook = rook;
+                this.knight = knight;
+                this.bishop = bishop;
+                this.queen = queen;
+                this.king = king;
             }
-        }
 
-        private int queens;
+            private int pawn;
 
-        public int Queens
-        {
-            get { return queens; }
-            set { 
-                if (value == (queens - 1)){
-                    queens--;
-                }
+            public int Pawn
+            {
+                get { return pawn; }
+                private set { pawn = value; }
             }
-        }
 
-        private int kings;
+            private int rook;
 
-        public int Kings
-        {
-            get { return kings; }
-            set { 
-                if (value == (kings - 1)){
-                    kings--;
-                }
+            public int Rook
+            {
+                get { return rook; }
+                private set { rook = value; }
+            }
+
+            private int knight;
+
+            public int Knight
+            {
+                get { return knight; }
+                private set { knight = value; }
+            }
+
+            private int bishop;
+
+            public int Bishop
+            {
+                get { return bishop; }
+                private set { bishop = value; }
+            }
+
+            private int queen;
+
+            public int Queen
+            {
+                get { return queen; }
+                private set { queen = value; }
+            }
+
+            private int king;
+
+            public int King
+            {
+                get { return king; }
+                private set { king = value; }
             }
         }
     }
+
+        
 }
