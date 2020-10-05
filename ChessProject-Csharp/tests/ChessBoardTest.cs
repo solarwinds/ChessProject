e1f9c839b91488eccdafa2399bb7ea2e8ddf1e8c
Taylor Rogers <taylor.rogers71@gmail.com>
2020-10-05 10:46:50 -0700

    ChessBoardTest: Refactor and add tests

--- ChessProject-Csharp/tests/ChessBoardTest.cs
+++ ChessProject-Csharp/tests/ChessBoardTest.cs
@@ -1,4 +1,18 @@
 ﻿using NUnit.Framework;
+using System;
+
+/*
+Things to test:
+- Cannot move past another piece (unless a Knight)
+- Cannot move off the board
+- Cannot Capture off the board
+- Cannot perform invalid move
+- Cannot perform invalid capture
+- Can create custom board setup
+- Cannot create a custom invalid board (size/dimensions)
+- Can have multiple piece types on the board
+- Can add multiple kinds of pieces to the board
+*/
 
 namespace SolarWinds.MSP.Chess
 {
@@ -79,29 +93,76 @@ namespace SolarWinds.MSP.Chess
 		{
 			Pawn firstPawn = new Pawn(PieceColor.Black);
 			Pawn secondPawn = new Pawn(PieceColor.Black);
-			chessBoard.Add(firstPawn, 6, 3, PieceColor.Black);
-			chessBoard.Add(secondPawn, 6, 3, PieceColor.Black);
+			chessBoard.Add(firstPawn, 6, 3);
+			try 
+			{
+				chessBoard.Add(secondPawn, 6, 3);
+				Assert.Fail("Chessboard was able to add a piece to an already-occupied position.");
+			} 
+			catch (DuplicatePositioningException)
+			{/* pass */}
 			Assert.AreEqual(firstPawn.XCoordinate, 6);
             Assert.AreEqual(firstPawn.YCoordinate, 3);
             Assert.AreEqual(secondPawn.XCoordinate, -1);
             Assert.AreEqual(secondPawn.YCoordinate, -1);
 		}
 
-        [Test]
-		public void Limits_The_Number_Of_Pawns()
+		[Test]
+		public void Avoids_Invalid_Positioning()
 		{
-			for (int i = 0; i < 10; i++)
+			Pawn firstPawn = new Pawn(PieceColor.Black);
+			Pawn secondPawn = new Pawn(PieceColor.Black);
+			chessBoard.Add(firstPawn, 6, 3);
+			try 
+			{
+				chessBoard.Add(secondPawn, 10, 3);
+				Assert.Fail("Chessboard was able to add a piece to an invalid position.");
+			} 
+			catch (InvalidPositioningException)
+			{/* pass */}
+			Assert.AreEqual(firstPawn.XCoordinate, 6);
+            Assert.AreEqual(firstPawn.YCoordinate, 3);
+            Assert.AreEqual(secondPawn.XCoordinate, -1);
+            Assert.AreEqual(secondPawn.YCoordinate, -1);
+		}
+
+		[Test]
+		public void IsPieceAvailable_Fails_When_Unavailable()
+		{
+			Pawn pawn = new Pawn(PieceColor.Black);
+			for (int i = 0; i < 10; i++){
+				if (i < 8)
+				{
+					Assert.IsTrue(chessBoard.IsPieceAvailable(pawn));
+					// chessBoard.AvailablePiecesBlack.Pawns--;
+				} else {
+					Assert.IsFalse(chessBoard.IsPieceAvailable(pawn));
+				}
+			}
+		}
+
+        [Test]
+		public void Add_Limits_The_Number_Of_Pawns()
+		{
+			for (int i = 0; i <= ChessBoard.MaxBoardHeight; i++)
 			{
 				Pawn pawn = new Pawn(PieceColor.Black);
 				int row = i / ChessBoard.MaxBoardWidth;
-				chessBoard.Add(pawn, 6 + row, i % ChessBoard.MaxBoardWidth, PieceColor.Black);
+				Console.WriteLine("Row: {0}, Col: {1}", (6 + row), i % ChessBoard.MaxBoardWidth);
 				if (row < 1)
 				{
+					chessBoard.Add(pawn, (6 + row), i % ChessBoard.MaxBoardWidth);
 					Assert.AreEqual(pawn.XCoordinate, (6 + row));
 					Assert.AreEqual(pawn.YCoordinate, (i % ChessBoard.MaxBoardWidth));
 				}
 				else
 				{
+					try 
+					{
+						chessBoard.Add(pawn, 6 + row, i % ChessBoard.MaxBoardWidth);
+					} 
+					catch (InvalidPositioningException)
+					{}
 					Assert.AreEqual(pawn.XCoordinate, -1);
                     Assert.AreEqual(pawn.YCoordinate, -1);
 				}
