9fb64235d47a5b19da2bbf77f4f906f869f2bdd1
Claudiu Cotuna <co.claudiu@icloud.com>
2021-01-26 09:03:46 +0200

    Finishing implementation

---
+++ ChessProject-Java/src/main/java/com/solarwindsmsp/chess/Piece.java
@@ -0,0 +1,50 @@
+package com.solarwindsmsp.chess;
+
+public class Piece {
+
+    private ChessBoard chessBoard;
+    private int xCoordinate;
+    private int yCoordinate;
+    private PieceColor pieceColor;
+
+    public Piece(PieceColor pieceColor) {
+        this.pieceColor = pieceColor;
+    }
+
+    public ChessBoard getChessBoard() {
+        return chessBoard;
+    }
+
+    public void setChessBoard(ChessBoard chessBoard) {
+        this.chessBoard = chessBoard;
+    }
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setXCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setYCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
+    public PieceColor getPieceColor() {
+        return pieceColor;
+    }
+
+    public void setPieceColor(PieceColor pieceColor) {
+        this.pieceColor = pieceColor;
+    }
+
+    public void invalidatePiece() {
+        this.xCoordinate = -1;
+        this.yCoordinate = -1;
+    }
+}
