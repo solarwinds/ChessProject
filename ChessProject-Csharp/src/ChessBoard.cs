1302a84ce8e444b60eb44d2cc811119114d56059
scott <scott@DESKTOP-311J2C9>
2021-06-28 20:52:59 +0100

    Implement ChessBoard.Add() and ChessBoard.IsLegalBoardPosition methods to get all ChessBoardTests passing

--- ChessProject-Csharp/src/ChessBoard.cs
+++ ChessProject-Csharp/src/ChessBoard.cs
@@ -15,12 +15,30 @@ namespace SolarWinds.MSP.Chess
 
         public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor)
         {
-            throw new NotImplementedException("Need to implement ChessBoard.Add()");
+            if (IsLegalBoardPosition(xCoordinate, yCoordinate))
+            {
+                pawn.XCoordinate = xCoordinate;
+                pawn.YCoordinate = yCoordinate;
+                
+                pieces[xCoordinate, yCoordinate] = pawn;
+            }
+            else
+            {
+                pawn.XCoordinate = -1;
+                pawn.YCoordinate = -1;
+            }
         }
+            
 
         public bool IsLegalBoardPosition(int xCoordinate, int yCoordinate)
         {
-            throw new NotImplementedException("Need to implement ChessBoard.IsLegalBoardPosition()");
+            if ((xCoordinate < 0 || xCoordinate >= MaxBoardWidth) || (yCoordinate < 0 || yCoordinate >= MaxBoardHeight))
+                return false;
+
+            if (pieces[xCoordinate, yCoordinate] != null)
+                return false;
+
+            return true;
         }
     }
 }
